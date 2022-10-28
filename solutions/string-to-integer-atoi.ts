6bc7ff548e4d9ee13f0c87a67c4c5bd6c0cc8474
Abigail Dustin <abigail.dustin.21@gmail.com>
2022-10-28 14:33:18 +0400

    Complete string-to-integer-atoi, all tests ✓

--- solutions/string-to-integer-atoi.ts
+++ solutions/string-to-integer-atoi.ts
@@ -1,3 +1,30 @@
+function checkByIndex(s: string, index: number, equalTo: string): boolean {
+	return ((index < s.length) && (s[index] === equalTo));
+}
+
 export function myAtoi(s: string): number {
-	return 0;
+	const digits = new Map([
+		["0",0],["1",1],["2",2],["3",3],["4",4],["5",5],["6",6],["7",7],["8",8],["9",9]
+	]);
+	let result = 0;
+	let index = 0;
+	let sign = 1;
+
+	while (index < s.length && s[index] === ' ') index++ ;
+	if (index < s.length && (s[index] === '+' || s[index] === '-')){
+		if (s[index] === '-') sign = -1 ;
+		index++;
+	}
+	while (s[index]) {
+		const digit = s[index];
+		let numberDigit: number | undefined;
+
+		if(!digits.has(digit)) break ;
+		numberDigit = digits.get(digit);
+		if (numberDigit !== undefined) result = (result * 10) + numberDigit;
+		if (result >= 2147483647 && sign === 1) return 2147483647;
+		if (result >= 2147483648 && sign === -1) return -2147483648;
+		index++ ;
+	}
+	return result * sign;
 };
